26c913d09d15544708c13b028cb9784279fae95b
Bram <bramreth@gmail.com>
2018-12-05 17:21:59 +0000

    added mqtt sqllite data commiting jazz, now all up and running.

--- flood_monitoring_system/models.py
+++ flood_monitoring_system/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MqttWaterLevelData(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    hardware_serial = models.CharField(max_length=20)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    latitude = models.DecimalField(max_digits=9, decimal_places=7)
+    altitude = models.IntegerField()
+    river_height_mm = models.CharField(max_length=6)  # models.IntegerField()
+
+
