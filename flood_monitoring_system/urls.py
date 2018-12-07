ab3db9a1ee6895b1af33786a580142f2eb959363
Sam Le-Cornu <sjl66@kentac.uk>
2018-12-07 16:53:19 +0000

    Big sammy wammy update

--- flood_monitoring_system/urls.py
+++ flood_monitoring_system/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url,include
-from itertools import chain
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
-    url(r'^index.html/$', views.index, name='index')
+    url(r'^index.html/$', views.index, name='index'),
+    url(r'^notifications/$', views.notifications, name='notifications'),
+    url(r'^test/$', views.test, name='test')
 ]
