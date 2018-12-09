396197d7f6864f01fb2dcbc607e1319273b641aa
Sam Le-Cornu <sjl66@kentac.uk>
2018-12-09 01:55:13 +0000

    Auto stash before merge of "master" and "origin/master"

--- flood_monitoring_system/views.py
+++ flood_monitoring_system/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from flood_monitoring_system.models import environmental_agency_flood_data, MqttWaterLevelData
+from flood_monitoring_system.models import environmental_agency_flood_data, MqttWaterLevelData, Notifications
 # Create your views here.
 query = {}
 query['api_data'] = environmental_agency_flood_data.get_newest("")
 print(query['api_data'])
 query['sensors'] = MqttWaterLevelData.get_newest("")
 query['sensors_all'] = MqttWaterLevelData.get_all("")
+query['notifications'] = Notifications.objects.all().order_by("-date")
 query['flood_area'] = []
+
 flood_area_coordinates = [
             [
               1.084671034348597,
@@ -1500,7 +1502,7 @@ def index(request):
     return render(request, 'flood_monitoring_system/index.html', {"object_list":query})
 
 def notifications(request):
-    return render(request, 'flood_monitoring_system/notifications.html')
+    return render(request, 'flood_monitoring_system/notifications.html', {"object_list":query['notifications']})
 
 def test(request):
     return render(request, 'flood_monitoring_system/test.html', {"object_list":query})
